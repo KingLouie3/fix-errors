c4681da0f13c8c0ea7db35dd9afc365898cbb5af
learn <learn@learn16.local>
2019-10-30 20:12:00 -0700

    Answer is lowercase the N in the people object

--- lesson6.js
+++ lesson6.js
@@ -1,13 +1,13 @@
-var people = [ 
-        { name: "John", age: 55 }, 
+var people = [
+        { name: "John", age: 55 },
         { name: "Roger", age: 44 },
         { Name: "Betty", age: 37 }
     ]
 
-//Desired result Print out all names 
-for ( var i = 0; i < people.length; i++  ){
-    let name = people[i].name
-    console.log("My Name is " + name )
+//Desired result Print out all names
+for ( var i = 0; i < people.length; i++  ) {
+    let name = people[i].name;
+    console.log("My Name is " + people[i].name );
 }
 
 console.log(name);
