859acb2e22d1931b0b2f5fa717fd39c406c418d2
learn <learn@learn16.local>
2019-10-30 20:02:08 -0700

    Answer is add a semicolon after array

--- lesson2.js
+++ lesson2.js
@@ -1,7 +1,9 @@
 //Should console.log all names in the name array
 
-var names = ["Alibaba", "Cassandra", "AJ"]
+var names = ["Alibaba", "Cassandra", "AJ"];
+
 for (var i = 0; i <= names.length; i++) {
     console.log("My Name is " + names[i])
 }
 
+//Answer is add a semicolon after the array
